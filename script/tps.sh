42d14a851767d4e445f193a21124c21d82767b4a
pis0 <msdraco@gmail.com>
2016-07-18 17:30:48 -0300

    dev

--- script/tps.sh
+++ script/tps.sh
@@ -5,7 +5,11 @@ set +v
 
 # CONFIG
 export TPS=$1
+
 export PRAIA_PATH=C:/workspace/Praia
+export MAGIC_PATH=C:/workspace/Magic
+export PIPAVB_PATH=C:/workspace/Pipavb
+
 export TEXTUREPACKER_PATH=TexturePackerGUI.exe
 
 # PROCESS
@@ -20,20 +24,27 @@ function fixPath()
 	i=1
 	for s in "${arr2[@]}"
 	do
-		if [[ ${s,,} =~ "praia" ]] 
+		if [[ ${s,,} =~ $2 ]] 
 		then	
 			arr3="${arr2[@]:$i}" 
 			arr4=(${arr3// //})
-			echo $PRAIA_PATH"/"${arr4[@]}
-			echo $(xmlstarlet ed -u $1 -v $PRAIA_PATH"/"${arr4[@]} "$TPS") > "$TPS"
+			echo $3"/"${arr4[@]}
+			echo $(xmlstarlet ed -u $1 -v $3"/"${arr4[@]} "$TPS") > "$TPS"
 			break 
 		fi
 		((i++))
 	done
+
 }
 
-fixPath "//struct[@type='Settings']/filename"[1]
-fixPath "//struct[@type='DataFile']"[1]"/filename"[1]
+fixPath "//struct[@type='Settings']/filename"[1] "praia" $PRAIA_PATH
+fixPath "//struct[@type='DataFile']"[1]"/filename"[1] "praia" $PRAIA_PATH
+
+fixPath "//struct[@type='Settings']/filename"[1] "magic" $MAGIC_PATH
+fixPath "//struct[@type='DataFile']"[1]"/filename"[1] "magic" $MAGIC_PATH
+
+fixPath "//struct[@type='Settings']/filename"[1] "pipavb" $PIPAVB_PATH
+fixPath "//struct[@type='DataFile']"[1]"/filename"[1] "pipavb" $PIPAVB_PATH  
  
 # RUN windows
 $TEXTUREPACKER_PATH "$TPS" 
